0cd029eeaab2e7f2b5b31f0b6201e5eb5968b9ed
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-09-25 09:50:32 -0700

    artist list

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+
+def about(request):
+	return render(request, 'about.html')
+
+
+def artist_index(request):
+	return render(request, 'artists/index.html', {'artists': artists})
+
+
+class Artist:
+	def __init__(self, monikr, artist_statement, genre, pronouns, medium):
+		self.monikr = monikr
+		self.artist_statement = artist_statement
+		self.genre = genre
+		self.pronouns = pronouns
+		self.medium = medium
+
+
+artists = [
+	Artist('sleepy icarus', 'comfort in loneliness', 'vaporwave', 'she/her', 'digital photography'),
+	Artist('kathryn mace-shanahan', 'quilting isn\'t boring', 'experimental', 'she/her', 'quilts'),
+	Artist('tima', 'cute things', '90\'s kid', 'she/her', 'laser cutting'),
+	Artist('tiza', 'i\'m a cool chick', 'lite goth', 'she/her', 'bending'),
+]
--- monikr/urls.py
+++ monikr/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('admin/', admin.site.urls),
+  # Add the line below...
+  path('', include('main_app.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
